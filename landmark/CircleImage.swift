a1f81f0a50183c0f54f743436a14776799664b32
Nikhil Kadyan <nikhilkadyan@icloud.com>
2022-06-04 21:59:14 +0530

    On Section 5: https://developer.apple.com/tutorials/swiftui/creating-and-combining-views

---
+++ landmark/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  landmark
+//
+//  Created by Nikhil Kadyan on 04/06/22.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
